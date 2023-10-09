dbf60cfdfafe9358484af52e94fe7c533ce49a86
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2023-10-10 00:43:11 +0530

    Time: 0 ms (100%), Space: 12.6 MB (7.14%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        int n=nums.size();
+        if(n==1){
+            return;
+        }
+
+        for(int i=n-2;i>=0;i--){
+            for(int j=n-1;j>i;j--){
+                if(nums[i]<nums[j]){
+                    swap(nums[j],nums[i]);
+                    sort(nums.begin()+i+1,nums.end());
+                    return;
+                }
+            }
+        }
+
+        sort(nums.begin(),nums.end());
     }
 };
