26994a9658a092e6de9a15b3acc1f7a1408a5683
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2023-10-10 01:39:56 +0530

    Time: 4 ms (47.42%), Space: 12.5 MB (18.96%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -5,17 +5,25 @@ public:
         if(n==1){
             return;
         }
-
+        int index=-1;
         for(int i=n-2;i>=0;i--){
-            for(int j=n-1;j>i;j--){
-                if(nums[i]<nums[j]){
-                    swap(nums[j],nums[i]);
-                    reverse(nums.begin()+i+1,nums.end());
-                    return;
-                }
+            if(nums[i]<nums[i+1]){
+                index=i;
+                break;
             }
         }
+        
+        if(index==-1){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
 
-        reverse(nums.begin(),nums.end());
+        for(int i=n-1;i>index;i--){
+            if(nums[i]>nums[index]){
+                swap(nums[i],nums[index]);
+                break;
+            }
+        }
+        reverse(nums.begin()+index+1,nums.end());
     }
 };
