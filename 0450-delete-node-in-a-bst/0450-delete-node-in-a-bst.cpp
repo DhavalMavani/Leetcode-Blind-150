fa3e031018a478fa80b35ce7795771e9205e8d9f
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2023-10-23 05:57:45 +0530

    Time: 19 ms (91.96%), Space: 32.9 MB (40.75%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -0,0 +1,67 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* ans=NULL;
+    void dfs(TreeNode* node,int key, TreeNode* parents){
+        if(node==NULL){
+            return;
+        }
+        if(node->val==key){
+
+            TreeNode* temp=node->left;
+                while(temp && temp->right){
+                    temp=temp->right;
+                }
+                if(temp && node->right){
+                    temp->right=node->right;
+                }else{
+
+                }
+                
+            if(parents==NULL){
+                if(temp){
+                    ans=node->left;
+                }
+                else{
+                    ans=node->right;
+                }
+            }
+            else{
+                if(temp){
+                    if(key < parents->val){
+                        parents->left=node->left;
+                    }
+                    else{
+                        parents->right=node->left;
+                    }
+                }
+                else{
+                    if(key < parents->val){
+                        parents->left=node->right;
+                    }
+                    else{
+                        parents->right=node->right;
+                    }
+                }
+            }
+        }
+        dfs(node->left,key,node);
+        dfs(node->right,key,node);
+    }
+
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        ans=root;
+        dfs(root,key,NULL);
+        return ans;
+    }
+};
