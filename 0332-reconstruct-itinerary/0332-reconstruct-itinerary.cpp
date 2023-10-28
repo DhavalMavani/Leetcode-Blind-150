c23ccb718fc1fb14d8e4b91701b6b1fbb717a5e5
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2023-10-28 16:25:24 +0530

    Time: 32 ms (8.43%), Space: 15.4 MB (31.28%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
@@ -1,27 +1,29 @@
 class Solution {
 
 public:
-    vector<string> ans;
-    void dfs(unordered_map<string, vector<string>> &adj, int  numOfTickets,string airport,vector<string> &res){
+    vector<string> dfs(unordered_map<string, vector<string>> &adj, int  numOfTickets,string airport,vector<string> &res){
         
         if(numOfTickets==0){
             res.emplace_back(airport);
-            ans=res;
-            return;
+            return res;
         }
         int n=adj[airport].size();
         for(int i=0;i<n;i++){
             res.emplace_back(airport);
             string temp=adj[airport][i];
             adj[airport].erase(adj[airport].begin()+i);
-            dfs(adj,numOfTickets-1,temp,res);
+
+            vector<string> ans=dfs(adj,numOfTickets-1,temp,res);
+            
             res.pop_back();
             adj[airport].insert(adj[airport].begin()+i,temp);
 
             if(!ans.empty()){
-                return;
+                return ans;
             }
         }
+
+        return {};
 
     }
 
@@ -34,8 +36,6 @@ public:
             numOfTickets++;
         }
         vector<string> res;
-        dfs(adj,numOfTickets,"JFK",res);
-        return ans;
-
+        return dfs(adj,numOfTickets,"JFK",res);
     }
 };
