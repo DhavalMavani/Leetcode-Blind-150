c1e4bd9b6c6260818e0e7cfda16fa397e3a376d4
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2023-11-01 23:15:14 +0530

    Time: 336 ms (31.64%), Space: 142.8 MB (62.48%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
@@ -2,13 +2,13 @@ class NumMatrix {
 public:
     vector<vector<int>> prefixSum;
     NumMatrix(vector<vector<int>>& matrix) {
-        vector<vector<int>> sum (matrix.size()+1,vector<int> (matrix[0].size()+1,0 ));
+        
+        prefixSum=vector<vector<int>> (matrix.size()+1,vector<int> (matrix[0].size()+1,0 ));;
         for(int i=0;i<matrix.size();i++){
             for(int j=0;j<matrix[0].size();j++){
-                sum[i+1][j+1]=sum[i+1][j]+sum[i][j+1]+matrix[i][j]-sum[i][j];
+                prefixSum[i+1][j+1]=prefixSum[i+1][j]+prefixSum[i][j+1]+matrix[i][j]-prefixSum[i][j];
             }
         }
-        prefixSum=sum;
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
