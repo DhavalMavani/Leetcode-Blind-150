fa339c86462f0b3b80a6704dd5d8c532417c8a2a
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2023-11-05 18:17:53 +0530

    Time: 97 ms (21.84%), Space: 30 MB (16.12%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-
-        int p_2=0;
-        int p_3=0;
-        int p_5=0;
-
-        vector<int> UglyNumber(n,1);
-
-        for(int i=1;i<n;i++){
-            int twoMultiple=2*UglyNumber[p_2];
-            int threeMultiple=3*UglyNumber[p_3];
-            int fiveMultiple=5*UglyNumber[p_5];
-
-            UglyNumber[i]=min(twoMultiple,min(threeMultiple,fiveMultiple));
-            
-            if(UglyNumber[i]==twoMultiple) p_2++;
-            if(UglyNumber[i]==threeMultiple) p_3++;
-            if(UglyNumber[i]==fiveMultiple) p_5++;
+        set<long> set;
+        long nthNumber = 1;
+        set.insert(nthNumber);
+        for(int i=0; i<n; i++){
+            nthNumber = *set.begin();
+            set.erase(nthNumber);
+            set.insert(nthNumber*2);
+            set.insert(nthNumber*3);
+            set.insert(nthNumber*5);
         }
-
-        return UglyNumber[n-1];
+        return nthNumber;
     }
 };
