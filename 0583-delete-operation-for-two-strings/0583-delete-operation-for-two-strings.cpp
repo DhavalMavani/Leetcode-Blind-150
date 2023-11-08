0ba7817e379d54359e647fcdfd7efce71d203e5f
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2023-11-08 14:40:46 +0530

    Time: 11 ms (87.31%), Space: 12.7 MB (21.09%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
-    int helper(string word1,string word2,int i1,int i2,vector<vector<int>>& dp){
-        if(i1==word1.size() || i2==word2.size()){
-            return 0;
-        }
-
-        if(dp[i1][i2]!=-1){
-            return dp[i1][i2];
-        }
-
-        if(word1[i1]==word2[i2]){
-            return dp[i1][i2] = 1+ helper(word1,word2,i1+1,i2+1,dp);
-        }
-
-        int a=helper(word1,word2,i1+1,i2,dp);
-        int b=helper(word1,word2,i1,i2+1,dp);
-
-        return dp[i1][i2]= max(a,b);
-    }
     int minDistance(string word1, string word2) {
-        int m=word1.size(),n=word2.size();
-        vector<vector<int>> dp (m,vector<int>(n,-1) );
-        return m+ n - (2* helper(word1,word2,0,0,dp));
+        int m = word1.length(), n = word2.length();
+        
+        int lcs = helper(word1, word2, m, n);
+        int steps = (m-lcs) + (n-lcs);
+        return steps;
+    }
+    
+    int helper(string word1, string word2, int m, int n){
+        vector<vector<int>> dp(m+1, vector<int>(n+1));
+        
+        //Intializing the matrix
+        for(int i=0; i<m+1; i++){
+            for(int j=0; j<n+1; j++){
+                if(i == 0 || j == 0)
+                    dp[i][j] = 0;
+            }
+        }
+        
+        //solving the subproblems
+        for(int i=1; i<m+1; i++){
+            for(int j=1; j<n+1; j++){
+                if(word1[i-1] == word2[j-1])
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                else
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[m][n];
     }
 };
