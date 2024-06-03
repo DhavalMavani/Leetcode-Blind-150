b901895422226ffe2e22b91788fbd8f8dd5e9469
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-06-04 01:59:44 +0530

    Time: 24 ms (63.32%), Space: 32 MB (83.33%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
-    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
-        // If both nodes exist, add their values then look at the next node.
+    TreeNode* mergeTrees(TreeNode* &root1, TreeNode* &root2) {
         if(root1!=NULL && root2!=NULL){
             root1->val += root2->val;
             root1->left=mergeTrees(root1->left,root2->left);
             root1->right=mergeTrees(root1->right,root2->right);
-        }else{//If one node doesn't exist,NOT null node will be used.
-            return root1? root1: root2;
-        }
-        //Return the first tree, which is now altered. No need to create a new node, which bloats memory.
+        }else return root1? root1: root2;
+
         return root1;
     }
 };
