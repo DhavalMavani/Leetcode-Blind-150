22fbdf8655b3ab447683d95e66cb063d12d36dd0
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-06-04 01:57:58 +0530

    Time: 25 ms (55.21%), Space: 32.1 MB (83.33%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
@@ -1,57 +1,15 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void dfs(TreeNode* &ans,TreeNode* l,TreeNode* r){
-        if(!l && !r) return;
-        if(!l){
-            ans->val=r->val;
-            if(r->left){
-                ans->left=new TreeNode();
-                dfs(ans->left,NULL,r->left);
-            }
-            if(r->right){
-                ans->right=new TreeNode();
-                dfs(ans->right,NULL,r->right);
-            }
-        }
-        else if(!r){
-            ans->val=l->val;
-            if(l->left){
-                ans->left=new TreeNode();
-                dfs(ans->left,l->left,NULL);
-            }
-            if(l->right){
-                ans->right=new TreeNode();
-                dfs(ans->right,l->right,NULL);
-            }
-        }
-        else{
-            ans->val=l->val+r->val;
-            if(l->left || r->left){
-                ans->left=new TreeNode();
-                dfs(ans->left,l->left,r->left);
-            }
-            if(l->right || r->right){
-                ans->right=new TreeNode();
-                dfs(ans->right,l->right,r->right);
-            }
-        }
-
-    }
     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
-        if(!root1 && !root2) return NULL;
-        TreeNode* ans=new TreeNode();
-        dfs(ans,root1,root2);
-        return ans;
+        // If both nodes exist, add their values then look at the next node.
+        if(root1!=NULL && root2!=NULL){
+            root1->val += root2->val;
+            root1->left=mergeTrees(root1->left,root2->left);
+            root1->right=mergeTrees(root1->right,root2->right);
+        }else{//If one node doesn't exist,NOT null node will be used.
+            return root1? root1: root2;
+        }
+        //Return the first tree, which is now altered. No need to create a new node, which bloats memory.
+        return root1;
     }
 };
