f2e4a36aa3fe82e56cb46ffaa804822887e1f65e
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-06-13 02:21:37 +0530

    Time: 17 ms (75.45%), Space: 12.5 MB (82.59%) - LeetHub

---
+++ 0877-shortest-path-visiting-all-nodes/0877-shortest-path-visiting-all-nodes.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int shortestPathLength(vector<vector<int>>& graph) {
+        int n=graph.size(), lim=(1<<n)-1,ans=-1;
+        vector<vector<bool>> visited(n,vector<bool>(lim,false));
+        queue<pair<int,int>> q;
+
+        for(int i=0;i<n;i++) q.push ({i,1<<i});
+        while(!q.empty()){
+            int l=q.size();
+            ans++;
+
+            for(int i=0;i<l;i++){
+                int node=q.front().first;
+                int mask=q.front().second;
+                int newMask= ((1<<node)|mask);
+                q.pop();
+
+                if(newMask==lim) return ans;
+
+                for(auto &adj: graph[node]){
+                    if(!visited[adj][newMask]){
+                        q.push({adj,newMask});
+                        visited[adj][newMask]=true;
+                    }
+                }
+            }
+
+        }
+        return ans;
+
+    }
+};
