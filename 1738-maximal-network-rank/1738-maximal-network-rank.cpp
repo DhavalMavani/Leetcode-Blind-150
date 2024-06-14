c4bb12d4e84029f055ffe3da856c0d8918754e96
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-06-15 01:24:10 +0530

    Time: 92 ms (27.51%), Space: 42.7 MB (37.75%) - LeetHub

--- 1738-maximal-network-rank/1738-maximal-network-rank.cpp
+++ 1738-maximal-network-rank/1738-maximal-network-rank.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-        int rank=0;
-
-        vector<vector<int>> adj(n);
-        for(auto &road: roads){
-            adj[road[0]].emplace_back(road[1]);
-            adj[road[1]].emplace_back(road[0]);
+        vector<int> degree(n, 0);
+        set<pair<int, int>> road_set;
+        
+        for (auto& road : roads) {
+            degree[road[0]]++;
+            degree[road[1]]++;
+            road_set.insert({road[0], road[1]});
+            road_set.insert({road[1], road[0]});
         }
 
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                int currRank=adj[i].size()+adj[j].size();
-                for(auto &k: adj[i]){
-                    if(k==j){
-                        currRank--;
-                        break;
-                    } 
+        int max_rank = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i+1; j < n; ++j) {
+                int rank = degree[i] + degree[j];
+                if (road_set.find({i, j}) != road_set.end()) {
+                    rank--;
                 }
-                rank=max(rank,currRank);
+                max_rank = max(max_rank, rank);
             }
         }
-        return rank;
+
+        return max_rank;
     }
 };
