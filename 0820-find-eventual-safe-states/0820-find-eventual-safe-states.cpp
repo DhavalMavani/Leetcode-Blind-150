7654b21f4be47c78f2837f341b22c53c2b8afc0a
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-06-23 00:46:29 +0530

    Time: 107 ms (83.47%), Space: 51.5 MB (80.92%) - LeetHub

---
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int a=0;
+
+    bool dfs(int node,vector<vector<int>>& graph, vector<int> &ans, vector<bool> &hasVisited,vector<bool> &isTerminal){
+        if(hasVisited[node]) return isTerminal[node];
+        hasVisited[node]=true;
+
+        for(auto &i: graph[node]){
+            if(!dfs(i,graph,ans,hasVisited,isTerminal)) return false;
+        }
+        isTerminal[node]=true;
+        ans.emplace_back(node);
+        return true;
+    }
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n=graph.size();
+        vector<bool> hasVisited(n,false);
+        vector<bool> isTerminal(n,false);
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            if(!hasVisited[i]) dfs(i,graph,ans,hasVisited,isTerminal);
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
