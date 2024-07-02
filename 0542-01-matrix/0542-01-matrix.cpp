d8e1a4fe6e4c49bffb4a653495dc5e73e51de77d
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-07-03 03:46:03 +0530

    Time: 43 ms (93.25%), Space: 33.9 MB (76.34%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int m=mat.size(),n=mat[0].size();
+        int m = mat.size(), n = mat[0].size();
         
-        vector<vector<int>> ans(m,vector<int>(n,-1));
-        int offset[]={0,1,0,-1,0};
-        queue<int> q;
+        vector<vector<int>> ans(m, vector<int>(n, -1));
+        int offset[] = {0, 1, 0, -1, 0};
+        queue<pair<int, int>> q;
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(mat[i][j]==0){
-                    ans[i][j]=0;
-                    q.emplace(i*n+j);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0) {
+                    ans[i][j] = 0;
+                    q.emplace(i, j);
                 }
             }
         }
 
-        int steps=1;
-        while(!q.empty()){
-            int s=q.size();
-            for(int i=0;i<s;i++){
-                int t=q.front();
+        int steps = 1;
+        while (!q.empty()) {
+            int s = q.size();
+            for (int i = 0; i < s; i++) {
+                auto [x, y] = q.front();
                 q.pop();
-                int x=t/n, y=t%n;
-                for(int j=0;j<4;j++){
-                    int a=x+offset[j], b=y+offset[j+1];
-                    if(a>=0 && b>=0 && a<m && b<n && ans[a][b]==-1){
-                        q.emplace(a*n+b);
-                        ans[a][b]=steps;
+
+                for (int j = 0; j < 4; j++) {
+                    int a = x + offset[j], b = y + offset[j + 1];
+
+                    if (a >= 0 && b >= 0 && a < m && b < n && ans[a][b] == -1) {
+                        ans[a][b] = steps;
+                        q.emplace(a, b);
                     }
                 }
             }
