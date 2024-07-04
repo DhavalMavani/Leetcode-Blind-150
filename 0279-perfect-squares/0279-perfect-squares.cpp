c1d0f2654835b6bb41fb48a7658fd13a57241e83
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-07-04 17:54:34 +0530

    Time: 147 ms (19.25%), Space: 9.9 MB (86.6%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     int helper(int res, vector<int> &dp){
-        if(res==0) return 0;
-        
-        if(dp[res]!=-1) return dp[res];
+        if(res==0){
+            return 0;
+        }
+        if(dp[res]!=-1){
+            return dp[res];
+        }
         int ans=INT_MAX;
-        for(int i=1;i*i<=res;i++) ans=min(ans,1+helper(res-i*i, dp) );
-
+        for(int i=1;i*i<=res;i++){
+            if(res-i*i>=0){
+                ans=min(ans,1+helper(res-i*i, dp) );
+            }            
+        }
         return dp[res]=ans;
     }
 
