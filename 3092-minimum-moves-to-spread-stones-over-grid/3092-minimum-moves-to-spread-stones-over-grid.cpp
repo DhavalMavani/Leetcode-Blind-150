9ff1ae592fc3921ce6fcf2fb71e1890285904f07
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-07-10 19:37:02 +0530

    Time: 7 ms (82.85%), Space: 21.3 MB (77.69%) - LeetHub

--- 3092-minimum-moves-to-spread-stones-over-grid/3092-minimum-moves-to-spread-stones-over-grid.cpp
+++ 3092-minimum-moves-to-spread-stones-over-grid/3092-minimum-moves-to-spread-stones-over-grid.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
-    void dfs(vector<int> &zero,vector<int> &extra,vector<vector<int>>& grid, int &moves, int ind, int currSteps){
+    vector<int> zero,extra;
+    int moves=1e9;
+    void dfs(vector<vector<int>>& grid, int ind, int currSteps){
         if(ind>=zero.size()){
             moves=min(moves,currSteps);
             return;
@@ -11,13 +13,12 @@ public:
             int x=extra[i]/3, y=extra[i]%3;
             if(grid[x][y]==1) continue;
             grid[x][y]--;
-            dfs(zero,extra,grid,moves,ind+1,currSteps+abs(x-a)+abs(y-b) );
+            dfs(grid,ind+1,currSteps+abs(x-a)+abs(y-b) );
             grid[x][y]++;
         }
     }
     int minimumMoves(vector<vector<int>>& grid) {
         int ans=0;
-        vector<int> zero,extra;
 
         for(int i=0;i<3;i++){
             for(int j=0;j<3;j++){
@@ -25,8 +26,7 @@ public:
                 else if(grid[i][j]>1) extra.emplace_back(i*3+j);
             }
         }
-        int moves=1e9;
-        dfs(zero,extra,grid,moves,0,0);
+        dfs(grid,0,0);
 
         return moves;
     }
