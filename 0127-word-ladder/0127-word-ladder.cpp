3fe6d38bb958401f48be3d7abbc7cbafa35e1e18
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-07-16 01:49:41 +0530

    Time: 68 ms (61.3%), Space: 19.5 MB (46.86%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,40 +1,41 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_map <string, unordered_set<string>> ump;
-        int wordsize=beginWord.size();
-        for(auto &word: wordList){
-            for(int i=0;i<wordsize;i++){
-                string temp=word;
-                temp[i]= '*';
-                ump[temp].emplace(word);
-            }
-        }
-        queue <string> q1;
-        unordered_map <string,int> hasVisited;
-        q1.emplace(beginWord);
-        int ctr=0;
-        while( !q1.empty() ){
-            int n=q1.size();
-            ctr++;
-            for(int i=0;i<n;i++){
-                for(int i=0;i<wordsize;i++){
-                    if(q1.front()==endWord){
-                        return ctr;
-                    }
-                    string t1=q1.front();
-                    t1[i]='*';
-                    for(auto word: ump[t1]){
-                        if(hasVisited[word]==1){
-                            continue;
+        unordered_set<string> dict;
+        for (int i = 0; i < wordList.size(); i++) dict.insert(wordList[i]);
+        
+        queue<string> q;
+        q.push(beginWord);
+        
+        int result = 1;
+        
+        while (!q.empty()) {
+            int count = q.size();
+            
+            for (int i = 0; i < count; i++) {
+                string word = q.front();
+                q.pop();
+                
+                if (word == endWord) return result;
+
+                dict.erase(word);
+                
+                for (int j = 0; j < word.size(); j++) {
+                    char c = word[j];
+                    for (int k = 0; k < 26; k++) {
+                        word[j] = k + 'a';
+                        if (dict.find(word) != dict.end()) {
+                            q.push(word);
+                            dict.erase(word);
                         }
-                        q1.emplace(word);
-                        hasVisited[word]=1;
                     }
+                    word[j] = c;
                 }
-                q1.pop();
             }
+            
+            result++;
         }
+        
         return 0;
     }
 };
