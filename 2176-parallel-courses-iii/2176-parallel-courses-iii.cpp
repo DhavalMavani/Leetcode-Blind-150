b2b13eb1506eab36fd9a98b221b2ea91aad4b461
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-07-20 01:09:03 +0530

    Time: 249 ms (96.8%), Space: 138.6 MB (56.28%) - LeetHub

---
+++ 2176-parallel-courses-iii/2176-parallel-courses-iii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int dfs(int node,vector<int> &timeTaken, vector<vector<int>> &adj,vector<int> &time){
+
+        if(timeTaken[node]!=-1) return timeTaken[node];
+        int curr=0;
+        for(auto &i: adj[node]){
+            int x=dfs(i,timeTaken,adj, time);
+            curr=max(curr,x);
+        }
+        return timeTaken[node]=curr+time[node-1];
+    }
+    int minimumTime(int n, vector<vector<int>>& relations, vector<int>& time) {
+        vector<vector<int>> adj(n+1);
+        for(auto &relation: relations) adj[relation[1]].emplace_back(relation[0]);
+
+        vector<int> timeTaken(n+1,-1);
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            if(timeTaken[i]==-1){
+                ans=max(ans,dfs(i, timeTaken, adj,time) );
+            }
+        }
+        return ans;
+    }
+};
