570360ab29d6c19456145ca864ec9eef489b6b86
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-07-19 17:13:08 +0530

    Time: 20 ms (38.44%), Space: 19 MB (53.76%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
@@ -1,41 +1,22 @@
 class Solution {
-
 public:
-    vector<string> dfs(unordered_map<string, vector<string>> &adj, int  numOfTickets,string airport,vector<string> &res){
-        
-        if(numOfTickets==0){
-            res.emplace_back(airport);
-            return res;
+    void dfs(unordered_map<string, vector<string>> &m, string airport, vector<string>& result) {
+        while (!m[airport].empty()) {
+            string next = m[airport].back();
+            m[airport].pop_back();
+            dfs(m, next, result);
         }
-        int n=adj[airport].size();
-        for(int i=0;i<n;i++){
-            res.emplace_back(airport);
-            string temp=adj[airport][i];
-            adj[airport].erase(adj[airport].begin()+i);
-
-            vector<string> ans=dfs(adj,numOfTickets-1,temp,res);
-            
-            res.pop_back();
-            adj[airport].insert(adj[airport].begin()+i,temp);
-
-            if(!ans.empty()){
-                return ans;
-            }
-        }
-
-        return {};
-
+        result.push_back(airport);
     }
 
     vector<string> findItinerary(vector<vector<string>>& tickets) {
         sort(tickets.begin(),tickets.end());
-        int numOfTickets=0;
-        unordered_map<string, vector<string>> adj; //adjacency_list
-        for(auto i: tickets){
-            adj[i[0]].emplace_back(i[1]);
-            numOfTickets++;
-        }
-        vector<string> res;
-        return dfs(adj,numOfTickets,"JFK",res);
+        unordered_map<string, vector<string>> m;
+        for (int i = tickets.size()-1; i >=0 ; i--) m[tickets[i][0]].push_back(tickets[i][1]);
+        
+        vector<string> result;
+        dfs(m, "JFK", result);
+        reverse(result.begin(), result.end());
+        return result;
     }
 };
