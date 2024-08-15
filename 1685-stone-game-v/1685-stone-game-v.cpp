313c18aa6ab9e333e23e966f3ea47674ddab1e00
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-08-16 03:27:13 +0530

    Time: 356 ms (78.02%), Space: 26.2 MB (44.93%) - LeetHub

--- 1685-stone-game-v/1685-stone-game-v.cpp
+++ 1685-stone-game-v/1685-stone-game-v.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
-    int calcScore(int l,int r,vector<int>& stoneValue,vector<vector<int>> &dp){
+    int calcScore(int l,int r,vector<int>& stoneValue,vector<vector<int>> &dp,vector<int> &prefixSum){
         if(l>=r) return 0;
         
         if(dp[l][r]!=INT_MIN) return dp[l][r];
-        int total=0;
-        for(int i=l;i<=r;i++) total+=stoneValue[i];
-
+        int total=prefixSum[r]-prefixSum[l]+stoneValue[l];
         int leftSum=0,rightSum=total, ans=INT_MIN;
 
         for(int i=l;i<r;i++){
             leftSum+=stoneValue[i];
             rightSum-=stoneValue[i];
             if(leftSum>rightSum){
-                ans=max(ans,rightSum+calcScore(i+1,r,stoneValue,dp));
+                ans=max(ans,rightSum+calcScore(i+1,r,stoneValue,dp,prefixSum));
             }
             else if(leftSum<rightSum){
-                ans=max(ans,leftSum+calcScore(l,i,stoneValue,dp));
+                ans=max(ans,leftSum+calcScore(l,i,stoneValue,dp,prefixSum));
             }
             else{
-                ans=max(ans,leftSum+calcScore(l,i,stoneValue,dp));
-                ans=max(ans,rightSum+calcScore(i+1,r,stoneValue,dp));
+                ans=max(ans,leftSum+calcScore(l,i,stoneValue,dp,prefixSum));
+                ans=max(ans,rightSum+calcScore(i+1,r,stoneValue,dp,prefixSum));
             }
         }
         return dp[l][r]=ans;
     }
     int stoneGameV(vector<int>& stoneValue) {
         int n=stoneValue.size();
+        vector<int> prefixSum(n,0);
+        prefixSum[0]=stoneValue[0];
+        for(int i=1;i<n;i++) prefixSum[i]=prefixSum[i-1]+stoneValue[i];
         vector<vector<int>> dp(n,vector<int>(n,INT_MIN));
-        return calcScore(0,n-1,stoneValue,dp);
+        return calcScore(0,n-1,stoneValue,dp,prefixSum);
     }
 };
