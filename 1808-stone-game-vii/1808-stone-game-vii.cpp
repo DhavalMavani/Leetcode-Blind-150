26bf9bb8a264ab7b5c3dd44b65fbe914a808f771
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-08-16 19:05:36 +0530

    Time: 166 ms (41.92%), Space: 87.1 MB (48.24%) - LeetHub

--- 1808-stone-game-vii/1808-stone-game-vii.cpp
+++ 1808-stone-game-vii/1808-stone-game-vii.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
-    int calcDiff(int l,int r,vector<int>& stones,vector<int> &prefix,vector<vector<int>> &dp){
+    int calcDiff(int l,int r,vector<int>& stones,int sum,vector<vector<int>> &dp){
         if(l>=r) return 0;
-        int total=prefix[r]-prefix[l]+stones[l];
         if(dp[l][r]!=INT_MIN) return dp[l][r];
-        int opt1= total-stones[l]-calcDiff(l+1,r,stones,prefix,dp);
-        int opt2= total-stones[r]-calcDiff(l,r-1,stones,prefix,dp);
+        int opt1= sum-stones[l]-calcDiff(l+1,r,stones,sum-stones[l],dp);
+        int opt2= sum-stones[r]-calcDiff(l,r-1,stones,sum-stones[r],dp);
         return dp[l][r]=max(opt1,opt2);
     }
     int stoneGameVII(vector<int>& stones) {
-        int n=stones.size();
-        vector<int> prefix(n);
-        prefix[0]=stones[0];
+        int n=stones.size(),sum=0;
         vector<vector<int>> dp(n,vector<int>(n,INT_MIN));
-        for(int i=1;i<n;i++) prefix[i]=prefix[i-1]+stones[i];
-        return calcDiff(0,n-1,stones,prefix,dp);
+        for(int i=0;i<n;i++) sum+=stones[i];
+        return calcDiff(0,n-1,stones,sum,dp);
     }
 };
