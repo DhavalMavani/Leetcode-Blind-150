a5e76590fa64467414d75fc3b457db7bfd390814
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-08-16 18:24:54 +0530

    Time: 162 ms (75.25%), Space: 117.7 MB (53.5%) - LeetHub

---
+++ 1788-stone-game-vi/1788-stone-game-vi.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int stoneGameVI(vector<int>& aliceValues, vector<int>& bobValues) {
+        int n=aliceValues.size(),alice=0,bob=0;
+        vector<pair<int,int>> arr;
+        for(int i=0;i<n;i++) arr.push_back({aliceValues[i]+bobValues[i],i});
+        sort(arr.begin(),arr.end(),greater<>());
+
+        for(int i=0;i<n;i++){
+            if(i%2) bob+=bobValues[arr[i].second];
+            else alice+=aliceValues[arr[i].second];
+        }
+        if(alice>bob) return 1;
+        else if(alice<bob) return -1;
+        return 0;
+    }
+};
