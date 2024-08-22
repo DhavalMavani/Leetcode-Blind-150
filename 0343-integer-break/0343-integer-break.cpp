f7f9dfd81e2574ec12556be107dd838f5fd7cbc1
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-08-23 02:24:47 +0530

    Time: 3 ms (36.81%), Space: 7.7 MB (18.51%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
-    int solve(int n,vector<int> &dp){
-        if(n<=3) return n;
-        int ans=1;
-        if(dp[n]!=-1) return dp[n];
-        for(int i=1;i<=n-1;i++) ans= max(ans, i * solve(n-i, dp) );
-        return dp[n]=ans;
-    }
     int integerBreak(int n) {
-        vector<int> dp(n+1,-1);
-        if(n==2 || n==3) return n-1;
-        return solve(n,dp);
+        if (n <= 3) return n-1;
+        
+        int count_of_3s = n/3 , remainder = n % 3;
+        
+        if (remainder == 0)  return pow(3, count_of_3s);
+        else if (remainder == 1) return pow(3, count_of_3s - 1) * 4;
+        else return pow(3, count_of_3s) * 2;
+
     }
 };
