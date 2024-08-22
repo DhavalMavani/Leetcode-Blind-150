261961c039300b65d996e5aea3bc7ce1400b7987
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-08-22 16:58:28 +0530

    Time: 39 ms (75.17%), Space: 28.4 MB (62.76%) - LeetHub

--- 0993-tallest-billboard/0993-tallest-billboard.cpp
+++ 0993-tallest-billboard/0993-tallest-billboard.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    int maxLen;
+    int sum;
     int solve(int index,int diff,vector<int>& rods,vector<vector<int>> &dp){
-        if(abs(diff)>2500) return INT_MIN;
+        if(abs(diff)>sum) return INT_MIN;
         if(index==rods.size()){
             if(diff==0)  return 0;
             return INT_MIN;
         }
-        if(dp[index][diff+2500]!=-1) return dp[index][diff+2500];
+        if(dp[index][diff+sum]!=-1) return dp[index][diff+sum];
 
         int dontAdd=solve(index+1,diff,rods,dp);
         int addLeft= rods[index] + solve(index+1,diff + rods[index] ,rods,dp);
         int addRight= rods[index] + solve(index+1,diff - rods[index] ,rods,dp);
 
-        return dp[index][diff+2500]=max(dontAdd,max(addLeft,addRight));
+        return dp[index][diff+sum]=max(dontAdd,max(addLeft,addRight));
     }
     int tallestBillboard(vector<int>& rods) {
         int n=rods.size();
-        vector<vector<int>> dp(n,vector<int>(5001,-1) );
+        sum = accumulate(rods.begin(), rods.end(), 0);
+        vector<vector<int>> dp(n,vector<int>(sum+1,-1) );
+        sum/=2;
         return solve(0,0,rods,dp)/2;
     }
 };
