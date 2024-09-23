144397b342534465bedcb41960936ba039f57247
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-09-23 12:10:39 +0530

    Time: 243 ms (51.8%), Space: 93.6 MB (82.57%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -4,13 +4,12 @@ public:
         sort(intervals.begin(),intervals.end());
         int temp=intervals[0][1],n=intervals.size(),ans=0,i=1;
 
-        while(i<n){
+        for(int i=1;i<n;i++){
             if(intervals[i][0]>=temp ) temp = intervals[i][1] ;
             else{
                 temp= min( temp, intervals[i][1]) ;
                 ans++;
             }
-            i++;
         }
         return ans;
     }
