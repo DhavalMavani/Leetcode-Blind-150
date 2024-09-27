307e1dd4aa3a9b8a5300278f6c93db920f65638e
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-09-27 11:35:00 +0530

    Time: 405 ms (92.38%), Space: 240.6 MB (71.82%) - LeetHub

---
+++ 1294-range-frequency-queries/1294-range-frequency-queries.cpp
@@ -0,0 +1,20 @@
+class RangeFreqQuery {
+private:
+    unordered_map<int, vector<int>> ump;
+public:
+    RangeFreqQuery(vector<int>& arr) {
+        for(int i=0;i<arr.size();i++) ump[arr[i]].emplace_back(i);
+    }
+    
+    int query(int left, int right, int value) {
+        auto l=lower_bound(ump[value].begin(),ump[value].end(),left );
+        auto r=upper_bound(ump[value].begin(),ump[value].end(),right );
+        return r-l;
+    }
+};
+
+/**
+ * Your RangeFreqQuery object will be instantiated and called as such:
+ * RangeFreqQuery* obj = new RangeFreqQuery(arr);
+ * int param_1 = obj->query(left,right,value);
+ */
