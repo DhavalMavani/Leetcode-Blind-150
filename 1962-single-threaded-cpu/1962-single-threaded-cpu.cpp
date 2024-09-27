b1bc19d0cb66b7758ded003e94aea892c4576748
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-09-27 15:06:08 +0530

    Time: 361 ms (64.46%), Space: 133.3 MB (48.87%) - LeetHub

--- 1962-single-threaded-cpu/1962-single-threaded-cpu.cpp
+++ 1962-single-threaded-cpu/1962-single-threaded-cpu.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     vector<int> getOrder(vector<vector<int>>& tasks) {
         int n=tasks.size(), ind=0;
+        long long currTime=0;
         for(int i=0;i<n;i++) tasks[i].emplace_back(i);
         sort(tasks.begin(),tasks.end());
-        long long currTime=tasks[0][0];
         
         vector<int> ans;
         priority_queue< pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>> > pq;
 
-        while(ans.size()!=n){
+        while(ind!=n || !pq.empty()){
             if(pq.empty()) currTime=max (currTime,(long long)tasks[ind][0]);
 
             while(ind<n && tasks[ind][0]<=currTime){
