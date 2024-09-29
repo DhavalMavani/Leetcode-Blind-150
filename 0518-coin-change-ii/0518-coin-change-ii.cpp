f6074729bdcfbc1f7a7f80303abd48d1a248ea0b
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-09-29 17:18:01 +0530

    Time: 33 ms (10.57%), Space: 27.3 MB (10.03%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,15 +1,13 @@
 class Solution {
 private:
     int combinations(vector<int>& coins,int target,int index,vector <vector<int>> &dp){
-
         if(index==coins.size()) return 0;
         if(target==0) return 1;
         if(dp[index][target]!=-1) return dp[index][target];
         
         int a=0,b=0;
-        b=combinations(coins,target,index+1,dp);
         if(target-coins[index]>=0) a=combinations(coins,target-coins[index],index,dp);
-        
+        b=combinations(coins,target,index+1,dp);
 
         return dp[index][target]=a+b;
     }
