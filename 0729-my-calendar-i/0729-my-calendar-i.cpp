56df0814f82bf3203e46e15596ea5ccc475b21c0
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-10-02 00:18:09 +0530

    Time: 65 ms (90.55%), Space: 42.7 MB (41.03%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.cpp
+++ 0729-my-calendar-i/0729-my-calendar-i.cpp
@@ -6,10 +6,10 @@ public:
         auto it= st.lower_bound({start,-1});
 
        // overlap with the next interval
-       if (it != st.end() && (*it).first<end) return 0;
+       if (it != st.end() && (*it).first<end) return false;
         
         // overlap with the previous interval
-        if (it != st.begin() && (prev(it))->second > start) return 0;
+        if (it != st.begin() && (prev(it))->second > start) return false;
 
         st.insert({start,end});
         return true;
