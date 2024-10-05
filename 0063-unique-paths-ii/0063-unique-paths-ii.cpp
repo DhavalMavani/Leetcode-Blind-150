4ef329924649e5c86010a3ed8a48fb6054733abb
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-10-05 15:56:41 +0530

    Time: 5 ms (36.63%), Space: 10.5 MB (40.64%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,30 +1,19 @@
 class Solution {
 public:
+    int row,col;
     int dfs(vector<vector<int>>& obstacleGrid,int r,int c,vector<vector<int>>& dp){
-
-        if(r==obstacleGrid.size()-1 && c==obstacleGrid[0].size()-1){
-            return 1;
-        }
-        if(dp[r][c]!=-1){
-            return dp[r][c];
-        }
+        if(r==row-1 && c==col-1) return 1;
+        if(dp[r][c]!=-1) return dp[r][c];
         int a=0,b=0;
-        if(r+1<obstacleGrid.size() && !obstacleGrid[r+1][c] ){
-            a=dfs(obstacleGrid,r+1,c,dp);
-        }
+        if(r+1<row && !obstacleGrid[r+1][c] ) a=dfs(obstacleGrid,r+1,c,dp);
 
-        if(c+1<obstacleGrid[0].size() && !obstacleGrid[r][c+1]){
-            b=dfs(obstacleGrid,r,c+1,dp);
-        }
+        if(c+1<col && !obstacleGrid[r][c+1]) b=dfs(obstacleGrid,r,c+1,dp);
 
         return dp[r][c]=a+b;
-
     }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        if( obstacleGrid[0][0] ){
-            return 0;
-        }
-
+        if( obstacleGrid[0][0] ) return 0;
+        row=obstacleGrid.size(), col=obstacleGrid[0].size();
         vector<vector<int>> dp(obstacleGrid.size(),vector<int>(obstacleGrid[0].size(),-1));
         return dfs(obstacleGrid,0,0,dp);
     }
