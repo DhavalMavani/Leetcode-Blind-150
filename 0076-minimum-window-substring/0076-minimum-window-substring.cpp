e8cf84fa8c4c9f7f768b208be1971df2d1d5b299
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-10-06 15:19:29 +0530

    Time: 20 ms (61.75%), Space: 10.9 MB (63.7%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int n=s.size(), requiredChars=t.size();
+        if(n<requiredChars) return "";
+    
+        unordered_map<char,int> ump;
+        for(auto &i: t) ump[i]++;
+
+        int ansInd=0, ansLen=1e9, l=0,r=0, currChars=0;
+
+        while(r<n && currChars<requiredChars){
+            if(ump[s[r]]>0) currChars++;
+            ump[s[r]]--;
+            r++;
+            while(currChars==requiredChars){
+                // int currLen=r-l;
+                if(r-l<ansLen){
+                    ansLen=r-l;
+                    ansInd=l;
+                }
+                ump[s[l]]++;
+                if(ump[s[l]]>0) currChars--;
+                l++;
+            }
+        }
+        if(ansLen==1e9) return "";
+        return s.substr(ansInd,ansLen);
+    }
+};
