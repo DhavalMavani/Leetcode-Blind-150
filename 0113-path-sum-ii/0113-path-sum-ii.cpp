d70d0e1ba85d5de2278b16404a2ed05b453973b7
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-10-14 11:44:50 +0530

    Time: 7 ms (70.5%), Space: 21.9 MB (48.54%) - LeetHub

---
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> isPossible(TreeNode* node,int target){
+        if(node==NULL) return {};
+        if(!node->left && !node->right){
+            if(target-node->val==0) return {{node->val}};
+            return {};
+        }
+        vector<vector<int>> l=isPossible(node->left,target-node->val);
+        vector<vector<int>> r=isPossible(node->right,target-node->val);
+        if(!l.empty()) for(auto &i: l) i.emplace_back(node->val);
+        if(!r.empty()) for(auto &i: r) i.emplace_back(node->val);
+
+        l.insert(l.end(), r.begin(), r.end());
+        return l;
+    }
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        vector<vector<int>> ans=isPossible(root,targetSum);
+        for(auto &i: ans) reverse(i.begin(),i.end());
+        return ans;
+    }
+};
