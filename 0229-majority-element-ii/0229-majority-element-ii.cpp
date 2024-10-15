89c78098444e9305b7a113823338a75aecacdba4
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-10-15 14:43:20 +0530

    Time: 12 ms (30.91%), Space: 19.9 MB (14.47%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,52 +1,14 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        int n=nums.size()/3;
         
-        int n=nums.size();
-        int ct1=0,ct2=0;
-        int ele1=INT_MIN, ele2=INT_MIN;
-
-        for(int i=0;i<n;i++){
-            if(ct1==0 && nums[i]!=ele2){
-                ele1=nums[i];
-                ct1++;
-            }
-            else if(ct2==0 && nums[i]!=ele1){
-                ele2=nums[i];
-                ct2++;
-            }
-            else if(nums[i]==ele1){
-                ct1++;
-            }
-            else if(nums[i]==ele2){
-                ct2++;
-            }
-            else{
-                ct1--;
-                ct2--;
-            }
-        }
-        
-        ct1=0;
-        ct2=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]==ele1){
-                ct1++;
-            }
-            else if(nums[i]==ele2){
-                ct2++;
-            }
-        }
+        unordered_map <int,int> ump;
+        for(auto &i: nums) ump[i]++;
 
         vector<int> ans;
+        for(auto &i: ump) if(i.second>n) ans.emplace_back(i.first);
 
-        if(ct1>n/3){
-            ans.emplace_back(ele1);
-        }
-        if(ct2>n/3){
-            ans.emplace_back(ele2);
-        }
-        
         return ans;
     }
 };
