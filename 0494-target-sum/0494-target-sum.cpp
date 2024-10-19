1fe305abe2eaa098514d705491eb4266cb9ef5ae
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-10-20 00:55:48 +0530

    Time: 17 ms (49.4%), Space: 40.3 MB (21.44%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,20 +1,23 @@
 class Solution {
-public:
-    int ans=0;
-    void helper(vector<int>& nums,int& target,int sum,int index){
-        if(index>=nums.size()){
-            if(sum==target){
-                ans++;
-            }
-            return;
+private:
+    int ans;
+    vector <vector<int>> dp;
+    int targetSum (vector<int>& nums, int target, int index){
+        if(index==nums.size()){
+            if(target==ans) return 1;
+            return 0;
         }
-        helper(nums,target,sum+nums[index],index+1);
-        helper(nums,target,sum-nums[index],index+1);
+        if(dp[index][target+1000]!=-1) return dp[index][target+1000];
+        
+        int a=targetSum(nums,target-nums[index],index+1 );
+        int b=targetSum(nums,target+nums[index],index+1 );
+
+        return dp[index][target+1000]=a+b;
     }
+public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        helper(nums,target,0,0);
-        return ans;
+        ans=target;
+        dp.resize(nums.size(), vector <int> (2000,-1) ); 
+        return targetSum(nums,0,0);
     }
 };
-
-
