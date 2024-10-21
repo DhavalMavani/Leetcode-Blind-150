2ac563881f2d6c70c731e76ce6e515ff19927904
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-10-21 16:17:00 +0530

    Time: 199 ms (50.38%), Space: 12.4 MB (33.6%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,23 +1,19 @@
 class Solution {
-
 public:
     vector<vector<int>> dp;
-    int maxCoinsCollected(vector<int>& nums,int left,int right){
+    int helper(int l,int r,vector<int>& nums){
+        if(l>r) return 0;
 
-        if(left>right) return 0;
-
-        if(dp[left][right]!=-1) return dp[left][right];
-
-        int maxVal=INT_MIN;
-        for(int i=left;i<=right;i++) maxVal=max( nums[left-1]*nums[i]*nums[right+1] + maxCoinsCollected(nums,left,i-1) + maxCoinsCollected(nums,i+1,right),maxVal );
-
-        return dp[left][right]= maxVal;
+        if(dp[l][r]!=-1)return dp[l][r];
+        int ans=-1;
+        for(int i=l;i<=r;i++) ans=max(ans,nums[l-1]*nums[i]*nums[r+1] + helper(l,i-1,nums) + helper(i+1,r,nums) );
+        return dp[l][r]=ans;
     }
     int maxCoins(vector<int>& nums) {
         nums.emplace(nums.begin(),1);
         nums.emplace_back(1);
         int n=nums.size();
-        dp.resize(n,vector<int> (n,-1));
-        return maxCoinsCollected(nums,1,n-2);
+        dp.resize(n-1,vector<int>(n-1,-1));
+        return helper(1,nums.size()-2,nums);
     }
 };
