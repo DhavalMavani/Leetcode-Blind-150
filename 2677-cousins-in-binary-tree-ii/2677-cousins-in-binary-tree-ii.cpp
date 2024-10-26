5e2912f3c179bee15bc76e5fd4a9fe6636546a19
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-10-26 16:16:55 +0530

    Time: 47 ms (35.64%), Space: 332.2 MB (31.85%) - LeetHub

--- 2677-cousins-in-binary-tree-ii/2677-cousins-in-binary-tree-ii.cpp
+++ 2677-cousins-in-binary-tree-ii/2677-cousins-in-binary-tree-ii.cpp
@@ -11,39 +11,26 @@
  */
 class Solution {
 public:
-    vector<int> depthsum;
-    void solve(TreeNode* root, int d){
-        if(root==NULL) return;
-
-        if(d>=depthsum.size()) depthsum.emplace_back(root->val);
-        else depthsum[d] += root->val;
-
-        solve(root->left, d+1);
-        solve(root->right, d+1);
-    }
-    
-
-    void builtTree(TreeNode* node,int level){
-
-        int childSum=0;
-        if(node->left) childSum+=node->left->val;
-        if(node->right) childSum+=node->right->val;
-
-        if(node->left){
-            node->left->val=depthsum[level+1]-childSum;
-            builtTree(node->left,level+1);
-        }
-        if(node->right){
-            node->right->val=depthsum[level+1]-childSum;
-            builtTree(node->right,level+1);
-        }
-
-    }
-
     TreeNode* replaceValueInTree(TreeNode* root) {
-        solve(root,0);
-        root->val=0;
-        builtTree(root,0);
+        root->val = 0;
+        queue<TreeNode*> q;  q.push(root);
+        while(!q.empty()){
+            int n = q.size(), sum = 0;
+            vector<TreeNode*> buf;
+            while(n--){
+                TreeNode* node = q.front(); q.pop();
+                buf.push_back(node);
+                if(node->left) { q.push(node->left); sum += node->left->val; }
+                if(node->right){ q.push(node->right); sum += node->right->val; }
+            }
+            for(auto node: buf){
+                int  t = sum;
+                if(node->left)  t -= node->left->val;
+                if(node->right) t -= node->right->val;
+                if(node->left)  node->left->val = t;
+                if(node->right) node->right->val = t;
+            }
+        }
         return root;
     }
 };
