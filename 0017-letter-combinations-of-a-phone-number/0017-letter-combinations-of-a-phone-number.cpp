09b4a564706f2d0a9bfd466c76f9e3159ac56bf3
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-10-27 15:43:35 +0530

    Time: 0 ms (100%), Space: 8.6 MB (26%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> keyPad={"","","abc","def","ghi","jkl", "mno","pqrs","tuv","wxyz" };
+    
+    void helper(string digits, int index,vector<string> &ans,string res){
+        if(index==digits.size()){
+            ans.emplace_back(res);
+            return;
+        }
+
+        for(auto &i: keyPad[ digits[index] -'0' ]){
+            helper(digits,index+1,ans,res+i);
+        }
+    }
+    vector<string> letterCombinations(string digits) {
+        vector<string> ans;
+        if(digits.empty()){
+            return ans;
+        }
+        helper(digits,0,ans,"");
+        return ans;
+    }
+};
