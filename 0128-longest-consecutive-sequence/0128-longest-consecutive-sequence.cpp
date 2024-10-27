bee9bbe3884f3a9f34d8bd2932c5da86b868dbb3
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-10-27 15:20:04 +0530

    Time: 142 ms (37.17%), Space: 100.2 MB (5.14%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
 int longestConsecutive(vector<int> &nums){
     int longest_cons_chain = 0;
-    unordered_map<int, bool> present;
-    unordered_map<int, bool> checked;
-    for (auto &a : nums) present[a] = true;
+    unordered_set<int> present;
+    unordered_set<int> checked;
+    for (auto &a : nums) present.emplace(a);
 
     for (int i = 0; i < nums.size(); i++)
     {
-        if (!checked[nums[i]] && !present.count(nums[i] - 1))
+        if ( !present.count(nums[i] - 1) && !checked.count(nums[i]))
         {
             int current_chain = 0, start = nums[i];
             while ( present.count(start) ){
                 current_chain++;
-                checked[start] = true;
+                checked.emplace(start);
                 start++;
             }
             longest_cons_chain = max(longest_cons_chain, current_chain);
