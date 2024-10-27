d003fc4b0526435bd8633de747b8bead9a0abb39
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-10-27 16:48:31 +0530

    Time: 15 ms (61.72%), Space: 11.4 MB (22.28%) - LeetHub

---
+++ 1263-number-of-dice-rolls-with-target-sum/1263-number-of-dice-rolls-with-target-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int K,m=1e9+7;
+    vector<vector<int>> dp;
+    int helper(int currSum,int n){
+        if(n==0){
+            if(currSum==0) return 1;
+            return 0;
+        }
+        if(currSum<=0) return 0;
+        if(dp[currSum][n]!=-1) return dp[currSum][n];
+        int ans=0;
+        for(int i=1;i<=K;i++){
+            ans=((long long) ans+helper(currSum-i,n-1))%m;
+        }
+        return dp[currSum][n]=ans;
+    }
+    int numRollsToTarget(int n, int k, int &target) {
+        K=k;
+        dp.resize(target+1,vector<int>(n+1,-1));
+        return helper(target,n);
+    }
+};
