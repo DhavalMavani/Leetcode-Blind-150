a0869781e45a62b1913fa6c0e90195023a680a0b
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-10-28 12:15:09 +0530

    Time: 5 ms (97.66%), Space: 15.6 MB (78.85%) - LeetHub

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,42 +1,30 @@
 class Solution {
 public:
-    int dijkstra(int node,unordered_map<int,vector<pair<int,int>>> &adj,int &distanceThreshold){
-        vector <bool> dist(adj.size(),false);
-        // pq<dist,node>
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>> > pq;
-        pq.push({0,node});
-        int ans=-1;
-        while(!pq.empty()){
-            int distance=pq.top().first;
-            int city=pq.top().second;
-            if(dist[city]) pq.pop();
-            else{
-                if(distance<=distanceThreshold) ans++;
-                else break;
-                dist[city]=true;
-                for(auto &i: adj[city]) pq.push({distance+i.second,i.first});
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector<vector<int>> dist(n,vector<int>(n,1e9));
+        for(int i=0;i<n;i++) dist[i][i]=0;
+
+        for(auto &i: edges) dist[i[0]][i[1]]=i[2],dist[i[1]][i[0]]=i[2];
+
+        for(int k=0;k<n;k++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    // if(dist[i][k]!=1e9 && dist[k][j]!=1e9){
+                        dist[i][j]=min(dist[i][j], dist[i][k]+dist[k][j] );
+                    // }
+                }
             }
         }
-        return ans;
-    }
-    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        // map[from]={{to,dist},{to,dist}}
-        unordered_map<int,vector<pair<int,int>>> adj;
-        for(auto &i: edges){
-            adj[i[0]].push_back({i[1],i[2]});
-            adj[i[1]].push_back({i[0],i[2]});
-        }
 
-        vector<int> citiesAtThreshold(n,0);
-        int num=INT_MAX;
+        int ans=-1,reachableCity=INT_MAX;
         for(int i=0;i<n;i++){
-            citiesAtThreshold[i]=dijkstra(i,adj,distanceThreshold);
-            num=min(citiesAtThreshold[i], num);
-        }
-        if(num==INT_MAX) return -1;
 
-        for(int i=n-1;i>=0;i--) if(citiesAtThreshold[i]==num) return i;
-        
-        return -1;
+            int count=-1;
+            for(int j=0;j<n;j++) if(dist[i][j]<=distanceThreshold) count++;
+
+            if(count<=reachableCity) ans=i,reachableCity=count;
+        }
+
+        return ans;
     }
 };
