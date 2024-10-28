4ef2c2ca71710790c295048b2b47d4371d573472
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-10-28 11:32:43 +0530

    Time: 87 ms (41.44%), Space: 12.5 MB (38.49%) - LeetHub

--- 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.cpp
+++ 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int K;
-    string str;
+    string* str;
     int helper(int l,int r){
 
         if(l>=r) return 0;
 
         vector<int> freq(26,0);
 
-        for(int i=l;i<=r;i++) freq[str[i]-'a']++;
+        for(int i=l;i<=r;i++) freq[(*str)[i]-'a']++;
 
-        for(int i=l;i<=r;i++) if(freq[str[i]-'a']<K) return max(helper(l,i-1),helper(i+1,r));
+        for(int i=l;i<=r;i++) if(freq[(*str)[i]-'a']<K) return max(helper(l,i-1),helper(i+1,r));
 
         return r-l+1;
 
@@ -18,7 +18,7 @@ public:
     int longestSubstring(string s, int k) {
         if(k==1) return s.size();
         K=k;
-        str=s;
+        str=&s;
         return helper(0,s.size()-1);
     }
 };
