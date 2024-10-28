153c2391ae805d0a5f0ff3a1d265a819d6a7bfc5
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-10-28 10:54:38 +0530

    Time: 3 ms (97.05%), Space: 22.2 MB (70.79%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.cpp
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
-        int n=word1.size(),n2=word2.size();
-        if(n!=n2) return false;
+        int n=word1.size();
+        if(n!=word2.size()) return false;
         
-        vector<int> freq1(26,0);
-        vector<int> freq2(26,0);
+        vector<int> freq1(26,0),freq2(26,0);
 
-        for(int i=0;i<n;i++){
-            freq1[word1[i]-'a']++;
-            freq2[word2[i]-'a']++;
-        }
+        for(int i=0;i<n;i++) freq1[word1[i]-'a']++,freq2[word2[i]-'a']++;
+
         for(int i=0;i<26;i++) if(freq1[i]==0 && freq2[i]!=0) return false;
-        sort(freq1.begin(),freq1.end(),greater());
-        sort(freq2.begin(),freq2.end(),greater());
 
+        sort(freq1.begin(),freq1.end());
+        sort(freq2.begin(),freq2.end());
 
-        for(int i=0;i<26;i++){
-            if(freq1[i]!=freq2[i]) return false;
-            else if(!freq1[i]) return true;
-        }
+        for(int i=0;i<26;i++) if(freq1[i]!=freq2[i]) return false;
 
         return true;
     }
