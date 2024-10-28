e801fb37df322f62592b3ef362dc60cda0241879
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-10-28 11:36:20 +0530

    Time: 1 ms (71.27%), Space: 9.6 MB (56.68%) - LeetHub

--- 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.cpp
+++ 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.cpp
@@ -3,16 +3,16 @@ public:
     int longestSubstring(string s, int k) {
         if (s.size() < k) return 0;
         
-        vector<int> freq(26);
-        for (char &c : s) freq[c-'a']++;
+        unordered_map<char, int> freq;
+        for (char c : s) freq[c]++;
         
         int i = 0;
-        while (i < s.size() && freq[s[i]-'a'] >= k) i++;
-
+        while (i < s.size() && freq[s[i]] >= k) i++;
+        
         if (i == s.size()) return s.size();
         
         int left = longestSubstring(s.substr(0, i), k);
-        while (i < s.size() && freq[s[i]-'a'] < k) i++;
+        while (i < s.size() && freq[s[i]] < k) i++;
         int right = longestSubstring(s.substr(i), k);
         
         return max(left, right);
