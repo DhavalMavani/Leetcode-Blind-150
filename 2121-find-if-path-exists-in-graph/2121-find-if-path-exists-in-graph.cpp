89c691bce157a78aade20e577f123725cfe0813e
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-10-28 17:36:10 +0530

    Time: 20 ms (98.03%), Space: 207.7 MB (98.62%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
-    vector<bool> visited;
-    vector<vector<int>> adj;
-    int d;
-    bool dfs(int node){
-        visited[node]=true;
-        if(node==d) return true;
-        for(auto &i: adj[node]) if(!visited[i] && dfs(i)) return true;
-        return false;
+    int find(int a[], int x) {
+        if (x != a[x]) a[x] = find(a, a[x]);
+        return a[x];
     }
+
+    void unionf(int a[], int x, int z) {
+        a[find(a, z)] = find(a, x);
+    }
+
+    bool connected(int a[], int x, int z) {
+        return find(a, x) == find(a, z);
+    }
+
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        visited.resize(n,false);
-        adj.resize(n);
-        d=source;
-
-        for(auto &i: edges) adj[i[0]].emplace_back(i[1]),adj[i[1]].emplace_back(i[0]);
-
-        return dfs(destination);
+        // unordered_map<int, vector<int>> mp
+        int p[n];
+        for (int i = 0; i < n; i++) p[i] = i;
+        for (auto& i : edges) {
+            unionf(p, i[0], i[1]);
+        }
+        return connected(p, source, destination);
     }
 };
