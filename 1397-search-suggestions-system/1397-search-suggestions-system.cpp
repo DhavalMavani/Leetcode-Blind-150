0bf015003e1195d2188043d21ed9013f6abc7a42
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-10-30 12:12:36 +0530

    Time: 12 ms (83.82%), Space: 33.2 MB (64.63%) - LeetHub

--- 1397-search-suggestions-system/1397-search-suggestions-system.cpp
+++ 1397-search-suggestions-system/1397-search-suggestions-system.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
 vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
-        sort(products.begin(),products.end());
+        auto start=products.begin();
+        sort(start,products.end());
         vector<vector<string>>ans;
+        vector<string>temp;
         string curr;
-
-        for(auto &c:searchWord){
+        for(auto c:searchWord){
             curr+=c;
-            vector<string>temp;
-            auto start=lower_bound(products.begin(),products.end(),curr);
-
+            temp.clear();
+            start=lower_bound(start,products.end(),curr);
             for(int i=0;i<3 && start+i!=products.end();i++){
                 string s=*(start+i);
-                if(s.find(curr)) break;
+                if(s.find(curr))break;
                 temp.push_back(s);
             }
             ans.push_back(temp);
