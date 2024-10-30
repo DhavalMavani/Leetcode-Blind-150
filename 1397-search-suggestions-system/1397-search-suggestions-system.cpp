03023083c8f6f008f9b7715966031abf514e657e
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-10-30 12:11:40 +0530

    Time: 15 ms (78.99%), Space: 33.8 MB (62.27%) - LeetHub

--- 1397-search-suggestions-system/1397-search-suggestions-system.cpp
+++ 1397-search-suggestions-system/1397-search-suggestions-system.cpp
@@ -1,66 +1,21 @@
-class TrieNode{
-public:
-    vector<TrieNode*> child;
-    bool isWord;
-    TrieNode(){
-        child.resize(26,NULL);
-        isWord=false;
-    }
-};
-
-class Trie{
-public:
-    TrieNode* node;
-    Trie(){ node=new TrieNode(); }
-
-    void insert(string &s){
-        TrieNode* curr=node;
-        int n=s.size();
-        for(int i=0;i<n;i++){
-            if(curr->child[s[i]-'a']==NULL ) curr->child[s[i]-'a']=new TrieNode();
-            curr=curr->child[s[i]-'a'];
-        }
-        curr->isWord=true;
-    }
-
-    vector<string> search(string &s){
-        TrieNode* curr=node;
-        int n=s.size();
-        for(int i=0;i<n;i++){
-            if(curr->child[s[i]-'a']==NULL ) return {};
-            curr=curr->child[s[i]-'a'];
-        }
-        vector<string> res;
-        dfs(curr,res,"");
-        for(auto &i: res) i=s+i;
-        return res;
-    }
-
-    void dfs(TrieNode* curr,vector<string> &res,string s){
-        if(res.size()==3) return;
-        if(curr->isWord) res.emplace_back(s);
-
-        for(int i=0;i<26;i++){
-            if(curr->child[i]!=NULL){
-                dfs(curr->child[i],res,s+char('a'+i));
-            }
-        }
-    }
-
-};
-
 class Solution {
 public:
-    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
-        Trie* root=new Trie();
-        for(auto &product: products) root->insert(product);
-        vector<vector<string>> ans;
-        int n=searchWord.size();
+vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+        sort(products.begin(),products.end());
+        vector<vector<string>>ans;
+        string curr;
 
-        string str;
-        for(auto &i: searchWord){
-            str+=i;
-            ans.emplace_back(root->search(str));
+        for(auto &c:searchWord){
+            curr+=c;
+            vector<string>temp;
+            auto start=lower_bound(products.begin(),products.end(),curr);
+
+            for(int i=0;i<3 && start+i!=products.end();i++){
+                string s=*(start+i);
+                if(s.find(curr)) break;
+                temp.push_back(s);
+            }
+            ans.push_back(temp);
         }
         return ans;
     }
