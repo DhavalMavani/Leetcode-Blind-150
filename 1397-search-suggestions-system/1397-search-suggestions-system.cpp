32d6050605ce8c668f3a43798dca2bca80282ca7
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-10-30 12:26:33 +0530

    Time: 4 ms (97.58%), Space: 27.2 MB (98.98%) - LeetHub

--- 1397-search-suggestions-system/1397-search-suggestions-system.cpp
+++ 1397-search-suggestions-system/1397-search-suggestions-system.cpp
@@ -1,32 +1,18 @@
 class Solution {
 public:
     vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+
         sort(products.begin(), products.end());
-        int a = 0;
-        int b = products.size()-1;
-        
+        int a=0, b=products.size()-1;
         vector<vector<string>> result(searchWord.size());
-        for (int i = 0; i < searchWord.size() && a <= b; ++i) {
-            while (
-                a <= b &&
-                // Skip products[a] if index i is not a match
-                (products[a].size() <= i || products[a][i] != searchWord[i])
-            ) {
-                ++a;
-            }
-            while (
-                a <= b &&
-                // Skip products[b] if index i is not a match
-                (products[b].size() <= i || products[b][i] != searchWord[i])
-            ) {
-                --b;
-            }
-            
-            // Pick the first 3 starting from a,
-            // but not going over b
-            for (int k = a; k <= b && k < a+3; ++k) {
-                result[i].push_back(products[k]);
-            }
+
+        for (int i=0; i<searchWord.size() && a<=b; i++) {
+
+            while( a<=b && (products[a].size() <= i || products[a][i] != searchWord[i]) ) a++;
+
+            while( a<=b && (products[b].size() <= i || products[b][i] != searchWord[i]) ) b--;
+
+            for (int k=a; k <= min(b,a+2); k++) result[i].push_back(products[k]);
         }
         return result;
     }
