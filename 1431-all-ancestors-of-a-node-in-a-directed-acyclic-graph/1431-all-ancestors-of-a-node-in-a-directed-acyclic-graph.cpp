0da404f6fc956a2eed5989884b66cfd0941b4d6b
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-10-30 15:37:15 +0530

    Time: 103 ms (67.72%), Space: 142.5 MB (54.75%) - LeetHub

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
+    void dfs(int node,vector<vector<int>> &adj, vector<int> &path,vector<int> &visited){
+        visited[node]=1;
+        for(auto &i: adj[node]) if(!visited[i]) dfs(i,adj,path,visited);
+        path.emplace_back(node);
+    }
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
         vector<vector<int>> ans(n);
         vector<vector<int>> adj(n);
-        vector<int> inDegree(n,0);
-        for(auto &edge: edges){
-            adj[edge[0]].emplace_back(edge[1]);
-            inDegree[edge[1]]++;
+        for(auto &edge: edges) adj[edge[1]].emplace_back(edge[0]);
+
+        for(int i=0;i<n;i++){
+            vector<int> path;
+            vector<int> visited(n,false);
+            for(auto &j: adj[i]) if(!visited[j]) dfs(j,adj,path,visited);
+            ans[i]=path;
+            sort(ans[i].begin(),ans[i].end());
         } 
-
-        queue<int> q;
-        for(int i=0;i<n;i++) if(inDegree[i]==0) q.emplace(i);
-
-        while(!q.empty()){
-            int curr=q.front();
-            q.pop();
-
-            for(auto &i: adj[curr]){
-                ans[i].insert(ans[i].begin(),ans[curr].begin(),ans[curr].end());
-                ans[i].emplace_back(curr);
-                sort( ans[i].begin(), ans[i].end() );
-                ans[i].erase( unique( ans[i].begin(), ans[i].end() ), ans[i].end() );
-                inDegree[i]--;
-                if(inDegree[i]==0) q.emplace(i);
-            }
-        }
 
         return ans;
     }
