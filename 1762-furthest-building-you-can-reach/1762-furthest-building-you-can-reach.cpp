90fe66b6cdcded621d8099cfe19c2c09b4c375bf
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-11-01 17:35:03 +0530

    Time: 14 ms (64.44%), Space: 58.1 MB (68.38%) - LeetHub

--- 1762-furthest-building-you-can-reach/1762-furthest-building-you-can-reach.cpp
+++ 1762-furthest-building-you-can-reach/1762-furthest-building-you-can-reach.cpp
@@ -7,14 +7,11 @@ public:
         for(int i=1;i<n;i++){
             int diff=heights[i-1]-heights[i];
             if(diff<0){
-                if(pq.size()<ladders) pq.emplace(diff);
-                else if(!pq.empty() && pq.top() > diff){
+                pq.emplace(diff);
+                if(pq.size()>ladders){
                     bricks+=pq.top();
                     pq.pop();
-                    pq.emplace(diff);
                 }
-                else bricks+=diff;
-
                 if(bricks<0) return i-1;
             }
         }
