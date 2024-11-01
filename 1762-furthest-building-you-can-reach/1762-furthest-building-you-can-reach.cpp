68784219ff15dd6e547aeebec12fb657d0e28165
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-11-01 17:24:33 +0530

    Time: 12 ms (74.81%), Space: 58.1 MB (49.33%) - LeetHub

--- 1762-furthest-building-you-can-reach/1762-furthest-building-you-can-reach.cpp
+++ 1762-furthest-building-you-can-reach/1762-furthest-building-you-can-reach.cpp
@@ -4,15 +4,15 @@ public:
         int n=heights.size();
         priority_queue< int >pq;
         for(int i=1;i<n;i++){
-            int diff=heights[i]-heights[i-1];
-            if(diff>0){
-                if(pq.size()<ladders) pq.emplace(-diff);
-                else if(!pq.empty() && (-pq.top()) < diff){
+            int diff=heights[i-1]-heights[i];
+            if(diff<0){
+                if(pq.size()<ladders) pq.emplace(diff);
+                else if(!pq.empty() && pq.top() > diff){
                     bricks+=pq.top();
                     pq.pop();
-                    pq.emplace(-diff);
+                    pq.emplace(diff);
                 }
-                else bricks-=diff;
+                else bricks+=diff;
 
                 if(bricks<0) return i-1;
             }
