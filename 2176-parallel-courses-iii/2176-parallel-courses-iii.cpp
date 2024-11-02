fa152c4974702f07dc32d4493ef5c484eb43bc73
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-11-02 19:05:27 +0530

    Time: 51 ms (63.69%), Space: 138.1 MB (56.54%) - LeetHub

--- 2176-parallel-courses-iii/2176-parallel-courses-iii.cpp
+++ 2176-parallel-courses-iii/2176-parallel-courses-iii.cpp
@@ -1,36 +1,22 @@
 class Solution {
 public:
+    int dfs(int node,vector<int> &timeTaken, vector<vector<int>> &adj,vector<int> &time){
+
+        if(timeTaken[node]!=-1) return timeTaken[node];
+        int curr=0;
+        for(auto &i: adj[node]){
+            int x=dfs(i,timeTaken,adj, time);
+            curr=max(curr,x);
+        }
+        return timeTaken[node]=curr+time[node-1];
+    }
     int minimumTime(int n, vector<vector<int>>& relations, vector<int>& time) {
-
-        vector<int> inDegree(n+1,0);
         vector<vector<int>> adj(n+1);
-        for(auto &relation: relations){
-            adj[relation[0]].emplace_back(relation[1]);
-            inDegree[relation[1]]++;
-        }
+        for(auto &relation: relations) adj[relation[0]].emplace_back(relation[1]);
 
-        vector<int> completionTime(n+1,0);
-        queue<int> q;
-
-        for(int i=1;i<=n;i++){
-            if(inDegree[i]==0){
-                q.emplace(i);
-                completionTime[i]=time[i-1];
-            }
-        }
-            
-        while(!q.empty()){
-            int curr=q.front();
-            q.pop();
-
-            for(auto &i: adj[curr]){
-                completionTime[i]=max(completionTime[i],completionTime[curr]+time[i-1]);
-                inDegree[i]--;
-                if(inDegree[i]==0) q.emplace(i);
-            }
-        }
-
-
-        return *max_element(completionTime.begin(), completionTime.end());;
+        vector<int> timeTaken(n+1,-1);
+        int ans=0;
+        for(int i=1;i<=n;i++) if(timeTaken[i]==-1) ans=max(ans,dfs(i, timeTaken, adj,time) );
+        return ans;
     }
 };
