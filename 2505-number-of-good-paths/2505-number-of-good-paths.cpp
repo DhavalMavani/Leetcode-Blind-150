b2971d4f702f3bd18042c285f7a0f9a7c662a264
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-11-06 02:23:42 +0530

    Time: 296 ms (63.43%), Space: 228.5 MB (43.48%) - LeetHub

--- 2505-number-of-good-paths/2505-number-of-good-paths.cpp
+++ 2505-number-of-good-paths/2505-number-of-good-paths.cpp
@@ -1,79 +1,82 @@
-class DisjointSet{
+#include <vector>
+#include <map>
+#include <unordered_map>
+using namespace std;
+
+class DisjointSet {
 public:
-    vector <int> size,parents;
-    DisjointSet(int n){
-        size.resize(n,1);
-        parents.resize(n);
-        for(int i=0;i<n;i++) parents[i]=i;
+    vector<int> size, parent;
+    
+    DisjointSet(int n) {
+        size.resize(n, 1);
+        parent.resize(n);
+        for (int i = 0; i < n; i++) parent[i] = i;
     }
 
-    int findUltimateParents(int a){
-        if(a==parents[a]) return a;
-        return parents[a] = findUltimateParents(parents[a]);
+    int findParent(int a) {
+        if (a == parent[a]) return a;
+        return parent[a] = findParent(parent[a]);
     }
-    void merge(int a, int b){
-        int UltimateParents_A=findUltimateParents(a);
-        int UltimateParents_B=findUltimateParents(b);
-        if(UltimateParents_A==UltimateParents_B) return;
 
-        if(size[UltimateParents_A] >= size[UltimateParents_B] ){
-            parents[UltimateParents_B]=UltimateParents_A;
-            size[UltimateParents_A]+=size[UltimateParents_B];
-        }
-        else{
-            parents[UltimateParents_A]=UltimateParents_B;
-            size[UltimateParents_B]+=size[UltimateParents_A];
+    void unionSets(int a, int b) {
+        int rootA = findParent(a);
+        int rootB = findParent(b);
+        if (rootA == rootB) return;
+
+        if (size[rootA] >= size[rootB]) {
+            parent[rootB] = rootA;
+            size[rootA] += size[rootB];
+        } else {
+            parent[rootA] = rootB;
+            size[rootB] += size[rootA];
         }
     }
 };
 
 class Solution {
 public:
-
     int nCr(int n, int r) {
         if (r > n) return 0;
-        if (r > n - r) r = n - r;  // Take advantage of symmetry, nCr == nC(n-r)
-
-        int result = 1;
+        if (r > n - r) r = n - r;
+        long long result = 1;
         for (int i = 0; i < r; i++) {
             result = result * (n - i) / (i + 1);
         }
-        return result;
+        return static_cast<int>(result);
     }
+    
     int numberOfGoodPaths(vector<int>& vals, vector<vector<int>>& edges) {
-        int n=vals.size(),ans=n;
-        DisjointSet* dsu= new DisjointSet(n);
+        int n = vals.size(), ans = n;
+        DisjointSet dsu(n);
         
         vector<vector<int>> adj(n);
+        map<int, vector<int>> valueGroups;
 
-        map<int,vector<int>> mp;    // val -> {nodes}
-        vector<bool> active(n,false);
-
-        for(auto &i:edges){
-            int e1=i[0], e2=i[1];
-            adj[e1].emplace_back(e2);
-            adj[e2].emplace_back(e1);
+        for (auto& edge : edges) {
+            int e1 = edge[0], e2 = edge[1];
+            adj[e1].push_back(e2);
+            adj[e2].push_back(e1);
         }
-        for (int i = 0; i < n; i++) mp[vals[i]].push_back(i); // Group nodes by their values
+        for (int i = 0; i < n; i++) valueGroups[vals[i]].push_back(i);
         
-        for(auto &i: mp){
-
-            for(auto &j: i.second){
-                active[j]=true;
-                for(auto &k: adj[j]){
-                    if(active[k]  && vals[k] <= i.first) dsu->merge(k,j);
+        vector<bool> active(n, false);
+        for (auto& [val, nodes] : valueGroups) {
+            for (int node : nodes) {
+                active[node] = true;
+                for (int &neighbor : adj[node]) {
+                    if (active[neighbor] && vals[neighbor] <= val) dsu.unionSets(neighbor, node);
                 }
             }
-
-            unordered_map<int,int> ump;
-            for(auto &j: i.second){
-                int p=dsu->findUltimateParents(j);
-                ump[p]++;
+            
+            unordered_map<int, int> componentSize;
+            for (int &node :nodes) {
+                int root = dsu.findParent(node);
+                componentSize[root]++;
             }
-
-            for(auto &j: ump) if(j.second>=2) ans+= nCr(j.second,2);
+            
+            for (auto& [root, size] : componentSize) if (size >= 2) ans += nCr(size, 2);
         }
-
+        
         return ans;
     }
 };
