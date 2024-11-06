9ae69f765664ae6925eb0df79865882e71f150f7
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-11-07 03:30:21 +0530

    Time: 7 ms (100%), Space: 13.9 MB (100%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -4,7 +4,9 @@ public:
         vector<int> ans(2,-1);
         int a=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
         int b=upper_bound(nums.begin(),nums.end(),target)-nums.begin();
-        if(a==nums.size() || nums[a]!=target  ) return ans;
+        if(a==nums.size() || nums[a]!=target  ){
+            return ans;
+        }
         ans[0]=a;
         ans[1]=b-1;
         return ans;
