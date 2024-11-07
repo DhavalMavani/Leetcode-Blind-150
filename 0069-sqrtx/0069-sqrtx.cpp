bb0c7301b43e31d03e0de2bdd41a3b49fd9210e0
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-11-07 16:07:34 +0530

    Time: 0 ms (100%), Space: 8.4 MB (18.19%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+        int l=1,r=x;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if((long long)mid*mid>x) r=mid-1;
+            else l=mid+1;
+        }
+        return r;
     }
 };
