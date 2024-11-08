05a07cf6a3be1d98885ba1dca078b8b0f679b2d1
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-11-09 03:36:12 +0530

    Time: 8 ms (61.14%), Space: 12.4 MB (93.09%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int swimInWater(vector<vector<int>>& grid) {
-        int n=grid.size();
+        int n=grid.size(), dest=n*n-1;
         vector<int> vis(n*n,false);
         priority_queue< pair<int,int> , vector<pair<int,int>>, greater<pair<int,int>> >pq;
         pq.push({grid[0][0],0});
-            vis[0]=true;
+        vis[0]=true;
         while(!pq.empty()){
             auto [time,cell]=pq.top();
             pq.pop();
-            if(cell==n*n-1) return time;
+            if(cell==dest) return time;
             int x=cell/n,y=cell%n;
             int dirs[]={0,-1,0,1,0};
             for(int i=0;i<4;i++){
