aa7a0c6837bbeb87ffeea1d88dc526e0d8c3f674
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-11-09 03:36:56 +0530

    Time: 3 ms (93.1%), Space: 12.5 MB (88.36%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int swimInWater(vector<vector<int>>& grid) {
-        int n=grid.size(), dest=n*n-1;
+        int n=grid.size();
         vector<int> vis(n*n,false);
         priority_queue< pair<int,int> , vector<pair<int,int>>, greater<pair<int,int>> >pq;
         pq.push({grid[0][0],0});
-        vis[0]=true;
+            vis[0]=true;
         while(!pq.empty()){
             auto [time,cell]=pq.top();
             pq.pop();
-            if(cell==dest) return time;
+            if(cell==n*n-1) return time;
             int x=cell/n,y=cell%n;
             int dirs[]={0,-1,0,1,0};
             for(int i=0;i<4;i++){
-                int nx=x+dirs[i],ny=y+dirs[i+1], nc=nx*n+ny;
-                if(nx>=0 && ny>=0 && nx<n && ny<n && !vis[nc]){
-                    vis[nc]=true;
-                    pq.push({max(grid[nx][ny],time),nc});
+                int nx=x+dirs[i],ny=y+dirs[i+1];
+                if(nx>=0 && ny>=0 && nx<n && ny<n && !vis[nx*n+ny]){
+                    vis[nx*n+ny]=true;
+                    pq.push({max(grid[nx][ny],time),nx*n+ny});
                 }
             }
         }
