4dd42ddbfffbff0a6eac2427fdbe70219bdb4c13
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-11-09 11:27:02 +0530

    Time: 36 ms (97.98%), Space: 39.9 MB (83.53%) - LeetHub

--- 0480-sliding-window-median/0480-sliding-window-median.cpp
+++ 0480-sliding-window-median/0480-sliding-window-median.cpp
@@ -3,9 +3,9 @@ public:
     vector<double> medianSlidingWindow(vector<int>& nums, int k) {
         int n = nums.size();
         vector<double> medians;
-        unordered_map<int,int> mp; // for late deletion
-        priority_queue<int> maxh; // max heap for lower half
-        priority_queue<int, vector<int>, greater<int>> minh; // min heap for upper half
+        unordered_map<int,int> mp; 
+        priority_queue<int> maxh;
+        priority_queue<int, vector<int>, greater<int>> minh;
         
         for(int i=0;i<k;i++) maxh.push(nums[i]);
 
@@ -15,34 +15,25 @@ public:
             if(k&1) medians.push_back(maxh.top()*1.0);
             else medians.push_back(((double)maxh.top()+(double)minh.top())/2);
 
-            int p=nums[i-k], q=nums[i]; 
-            // we need to remove p and add q;
-            // we will delete p when it will come on the top, to keep track we will maintain map
-            
-            int balance = 0; 
-            // keep heaps in balance, for correct ans
-            // we decrese balance when remove elements from maxh, so basically if balance<0 it means maxheap has lesser elemets the minheap
-            // removing p or adding p to map to delete it later
-            if(p<=maxh.top()) { // p is in maxheap
+            int p=nums[i-k], q=nums[i], balance = 0; 
+
+            if(p<=maxh.top()) {
                 balance--;
                 if(p==maxh.top()) maxh.pop();
                 else mp[p]++;
             }
-            else { // p is min heap
+            else {
                 balance++;
                 if(p == minh.top()) minh.pop();
                 else mp[p]++;
             }
             
-            // inserting q to the right heap
-            if(!maxh.empty() and q<=maxh.top()) maxh.push(q),balance++; // pushing q to maxheap
-            else minh.push(q),balance--; // pushing q to minheap
+            if(!maxh.empty() and q<=maxh.top()) maxh.push(q),balance++;
+            else minh.push(q),balance--;
             
-            // balancing both the heaps
             if(balance<0) maxh.push(minh.top()), minh.pop();
             else if(balance>0) minh.push(maxh.top()),maxh.pop();
             
-            // removing top elements if they exist in our map(late deletion)
             while(!maxh.empty() and mp[maxh.top()]) mp[maxh.top()]--,maxh.pop();
 
             while(!minh.empty() and mp[minh.top()]) mp[minh.top()]--,minh.pop();
