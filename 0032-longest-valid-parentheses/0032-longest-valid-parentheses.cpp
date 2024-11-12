733d53381c26cfdab0ee636f2be1ff57dff82c5f
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-11-12 11:21:02 +0530

    Time: 0 ms (100%), Space: 8.5 MB (96.52%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -6,16 +6,16 @@ public:
             if(s[i]=='(') left++;
             else right++;
 
-            if(right>left) left=0,right=0;
-            else if(left==right) ans=max(ans,2*right);
+            if(left==right) ans=max(ans,2*right);
+            else if(right>left) left=0,right=0;
         }
         left=0,right=0;
         for(int i=n-1;i>=0;i--){
             if(s[i]==')') right++;
             else left++;
 
-            if(left>right) left=0,right=0;
-            else if(left==right) ans=max(ans,2*left);
+            if(left==right) ans=max(ans,2*left);
+            else if(left>right) left=0,right=0;
         }
         return ans;
     }
