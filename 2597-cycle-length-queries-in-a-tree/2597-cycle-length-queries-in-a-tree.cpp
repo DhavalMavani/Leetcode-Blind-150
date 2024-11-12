061c152b7c99bccd0d34f47588b58f9a8048a158
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-11-12 15:46:22 +0530

    Time: 7 ms (98.82%), Space: 68.5 MB (84.8%) - LeetHub

--- 2597-cycle-length-queries-in-a-tree/2597-cycle-length-queries-in-a-tree.cpp
+++ 2597-cycle-length-queries-in-a-tree/2597-cycle-length-queries-in-a-tree.cpp
@@ -6,8 +6,8 @@ public:
         for(int i=0;i<s;i++){
             int steps=1,a=queries[i][0],b=queries[i][1];
             while(a!=b){
-                if(a>b) a/=2;
-                else b/=2;
+                if(a>b) a=a>>1;
+                else b=b>>1;
                 steps++;
             }
             ans[i]=steps;
