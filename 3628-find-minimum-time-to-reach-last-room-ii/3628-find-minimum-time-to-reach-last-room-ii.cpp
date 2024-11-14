14dc33a71af5c1e6c1f26ab690b859ff9cf62d58
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-11-14 17:04:21 +0530

    Time: 226 ms (91.28%), Space: 87.7 MB (99.43%) - LeetHub

--- 3628-find-minimum-time-to-reach-last-room-ii/3628-find-minimum-time-to-reach-last-room-ii.cpp
+++ 3628-find-minimum-time-to-reach-last-room-ii/3628-find-minimum-time-to-reach-last-room-ii.cpp
@@ -4,14 +4,14 @@ public:
         int r=moveTime.size(),c=moveTime[0].size();
         vector<vector<bool>> vis(r,vector<bool>(c,false));
         
-        priority_queue<vector<int>,vector<vector<int>>, greater<vector<int>> >pq;
-        pq.push({0,0,1});
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>> >pq;
+        pq.push({0,{0,1}});
         vis[0][0]=1;
         int dirs[]={0,1,0,-1,0};
 
         while(!pq.empty()){
-            vector<int> t=pq.top();
-            int time=t[0],cell=t[1],isOdd=t[2];
+            auto [time, nestedPair] = pq.top();
+            auto [cell, isOdd] = nestedPair;
             pq.pop();
             if(cell==r*c-1) return time;
             int x=cell/c,y=cell%c;
@@ -19,8 +19,8 @@ public:
                 int a=x+dirs[i],b=y+dirs[i+1];
                 if(a>=0 && b>=0 && a<r && b<c && !vis[a][b]){
                     vis[a][b]=true;
-                    if(isOdd) pq.push({1+max(time,moveTime[a][b]),a*c+b,!isOdd});
-                    else pq.push({2+max(time,moveTime[a][b]),a*c+b,!isOdd});
+                    if(isOdd) pq.push({1+max(time,moveTime[a][b]),{a*c+b,!isOdd}});
+                    else pq.push({2+max(time,moveTime[a][b]),{a*c+b,!isOdd}});
                 }
             }
 
