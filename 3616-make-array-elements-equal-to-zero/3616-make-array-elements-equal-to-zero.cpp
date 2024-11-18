67bd471471a629f7cc11fb9536a38be46a2bf8a5
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-11-18 08:10:53 +0530

    Time: 0 ms (100%), Space: 21.4 MB (58.33%) - LeetHub

--- 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.cpp
+++ 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
     int countValidSelections(vector<int>& nums) {
-        int ans=0,n=nums.size();
-        vector<int> prefix(n);
-        vector<int> sufix(n);
-
-        for(int i=1;i<n;i++) prefix[i]=prefix[i-1]+nums[i-1];
-        for(int i=n-2;i>=0;i--) sufix[i]=sufix[i+1]+nums[i+1];
-
-        for(int i=0;i<n;i++){
-            if(nums[i]==0){
-                if(sufix[i]==prefix[i]) ans+=2;
-                else if(abs(sufix[i]-prefix[i])==1) ans++;
+        int index=0, left=0, right=0,n=nums.size(),count = 0;
+        
+        for (int i=0; i<n; i++) {
+            left+=nums[i];
+            if (nums[i]==0) {
+                index = i;
+                break;
             }
         }
-
-        return ans;
+        
+        for (int i=index+1; i<n; i++) right += nums[i];        
+    
+        for (int i=index; i<n; i++) {
+            if (nums[i] != 0){
+                left += nums[i];
+                right -= nums[i];
+            }
+            else{
+                if (left == right) count+=2;
+                else if (abs(left-right)==1) count++;
+            }
+        }
+        
+        return count;
     }
 };
