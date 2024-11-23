583e042e545f9f7fb6bf7ae8492b1b033648495e
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-11-24 01:37:36 +0530

    Time: 511 ms (37.91%), Space: 11.8 MB (35.82%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -5,16 +5,15 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        vector<int> arr(n,-1);
+        vector<bool> arr(n,false);
+        int ans=0;
 
         for(int i=2;i<n;i++){
-            if(arr[i]==-1){
-                arr[i]=0;
-                for(int j=i+i;j<n;j+=i) arr[j]=1;
+            if(!arr[i]){
+                ans++;
+                for(int j=i;j<n;j+=i) arr[j]=1;
             }
         }
-        int ans=0;
-        for(int i=2;i<n;i++) if(!arr[i]) ans++;
 
         return ans;
     }
