adc391b31ae4c9fb02790d9e768e2e69fb795342
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-12-08 15:57:00 +0530

    Time: 317 ms (48.28%), Space: 120.7 MB (30.7%) - LeetHub

---
+++ 0802-k-th-smallest-prime-fraction/0802-k-th-smallest-prime-fraction.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> kthSmallestPrimeFraction(vector<int>& nums, int k) {
+        vector<pair<double,pair<int,int>> > arr;
+        int n=nums.size();
+
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                arr.push_back({double(nums[i])/nums[j],{i,j}});
+            }
+        }
+
+        sort(arr.begin(),arr.end());
+
+        return {nums[arr[k-1].second.first],nums[arr[k-1].second.second]};
+    }
+};
