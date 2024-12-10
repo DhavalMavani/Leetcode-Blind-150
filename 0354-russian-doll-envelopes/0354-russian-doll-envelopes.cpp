d5641ccbc624a88e9a206d135cf32b9ea545d980
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-12-10 14:44:49 +0530

    Time: 44 ms (68.87%), Space: 81.8 MB (44.62%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -7,9 +7,11 @@ public:
     int maxEnvelopes(vector<vector<int>>& envelopes) {
         sort(envelopes.begin(),envelopes.end(),comp);
         int n=envelopes.size();
-        vector<int> temp;
-        
+
+        vector<int> temp;        
         temp.emplace_back(envelopes[0][1]);
+
+        
         for(int i=1;i<n;i++){
             if(temp.back()<envelopes[i][1]) temp.emplace_back(envelopes[i][1]);
             else *lower_bound(temp.begin(),temp.end(),envelopes[i][1])= envelopes[i][1];
