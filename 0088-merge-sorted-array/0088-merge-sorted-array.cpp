4f84f0b4b24ba0117eb3dc6ca17622d6d38b38a1
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-12-13 15:24:09 +0530

    Time: 0 ms (100%), Space: 12.3 MB (9.59%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -4,14 +4,9 @@ public:
         int n1=m-1,n2=n-1,ind=m+n-1;
 
         while(n1>=0 && n2>=0){
-            if(nums1[n1]>nums2[n2]){
-                nums1[ind]=nums1[n1];
-                n1--;
-            } 
-            else{
-                nums1[ind]=nums2[n2];
-                n2--;
-            } 
+            if(nums1[n1]>nums2[n2]) nums1[ind]=nums1[n1], n1--;
+            else nums1[ind]=nums2[n2],n2--;
+            
             ind--;
         }
 
