8c6633c7a8329faefc011b0c44e7e045472d549f
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-12-15 04:11:23 +0530

    Time: 224 ms (72.8%), Space: 25.8 MB (63.86%) - LeetHub

---
+++ 0976-minimum-area-rectangle/0976-minimum-area-rectangle.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int calcDist(int x1, int y1, int x2, int y2) {
+        if (x1 == x2) return abs(y2 - y1);
+        else return abs(x2 - x1);
+    }
+    int minAreaRect(vector<vector<int>>& points) {
+        int n=points.size(),ans=INT_MAX;
+        unordered_map<int,unordered_set<int>> st;
+
+        for(int i=0;i<n;i++) st[points[i][0]].emplace(points[i][1]);
+
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int x1=points[i][0],y1=points[i][1],x2=points[j][0],y2=points[j][1];
+                if(x1==x2 || y1==y2) continue;
+
+                int p1x=points[i][0], p1y = points[j][1], p2x= points[j][0] ,p2y = points[i][1];
+
+                if(st.count(p1x) && st[p1x].count(p1y) && st.count(p2x) && st[p2x].count(p2y)){
+                    int area=calcDist(x1,y1,p1x,p1y)*calcDist(x1,y1,p2x,p2y);
+                    ans=min(ans,area);
+                }
+            }
+        }
+        if(ans==INT_MAX) return 0;
+        return ans;
+    }
+};
