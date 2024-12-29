7753ade3478652464cfe9dd17a952874b0f95835
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2024-12-29 16:33:33 +0530

    Time: 274 ms (22.28%), Space: 29.9 MB (37.2%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-
+        sort(nums.begin() , nums.end());
+        if(nums.size() < 3 || nums[0] > 0) return {};
+        unordered_map<int , int> hashMap;
         int n=nums.size();
-
-        vector<vector<int>> ans;
-
-        sort(nums.begin(),nums.end());
-        for (int i=0;i<n-2;i++){
-
-            if (i!=0 && nums[i]==nums[i-1]) continue;
-
-            int target =-nums[i], l=i+1, r=n-1;
-
-            while ( l<r){
-                if(nums[l]+nums[r]>target) r--;
-                else if(nums[l]+nums[r]<target) l++;
-                else{
-                    ans.push_back({nums[i],nums[l],nums[r]});
-                    
-                    while (l<r && nums[l] == nums[l+ 1]) l++;
-                    l++;
-                }
-            }
-        }
+        for(int i = 0 ; i<n ; i++) hashMap[nums[i]] = i;
         
-        return ans;
+        vector<vector<int>> answer;
+        for(int i = 0 ; i < nums.size() - 2 ; ++i){
+            if(nums[i] > 0) break;
+            
+            for(int j=i+1; j<n-1 ; j++){     //Fixing another number after first number
+                int required = -1*(nums[i] + nums[j]);    //To make sum 0, we would require the -ve sum of both fixed numbers.
+                if(hashMap.count(required) && hashMap.find(required)->second > j){ //If it exists in hashmap and its last occurrence index > 2nd fixed index, we found our triplet.
+                    answer.push_back({nums[i] , nums[j] , required});
+                }
+                j = hashMap.find(nums[j])->second; //Update j to last occurence of 2nd fixed number to avoid duplicate triplets.
+            }
+            i = hashMap.find(nums[i])->second;     //Update i to last occurence of 1st fixed number to avoid duplicate triplets.
+        }
+        return answer;  //Return answer vector.
     }
 };
