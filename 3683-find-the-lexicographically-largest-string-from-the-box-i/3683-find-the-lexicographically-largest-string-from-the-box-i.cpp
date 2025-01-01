575e87ea3b4e8bcbff3d2d3a71bc8c94fb481076
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-01-02 01:11:36 +0530

    Time: 138 ms (26.16%), Space: 258 MB (10.34%) - LeetHub

---
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string comp(string &a, string b){
+        int n1=a.size(),n2=b.size(), n=min(n1,n2), i=0;
+
+        while(i<n && a[i]==b[i]) i++;
+
+        if(i==n1 || i==n2){
+            if(n1>n2)return a;
+            else return b;
+        }
+        if((a[i]-'a')>(b[i]-'a')) return a;
+
+        return b;
+    }
+    string answerString(string word, int numFriends) {
+        int n=word.size(),windowSize=n-numFriends+1,ind=n-1;
+        if(numFriends==1) return word;
+        string ans="";
+
+        while(ind>=n-windowSize){
+            ans=comp(ans,word.substr(ind));
+            ind--;
+        }
+
+        while(ind>=0){
+            ans=comp(ans,word.substr(ind,windowSize));
+            ind--;
+        }
+
+        return ans;
+    }
+};
