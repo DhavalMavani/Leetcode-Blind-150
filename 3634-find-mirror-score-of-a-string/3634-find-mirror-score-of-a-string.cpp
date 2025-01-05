90a0237b2a370bc7267f111aae8edb89563bcf28
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-01-05 18:11:18 +0530

    Time: 9 ms (100%), Space: 16.5 MB (75%) - LeetHub

--- 3634-find-mirror-score-of-a-string/3634-find-mirror-score-of-a-string.cpp
+++ 3634-find-mirror-score-of-a-string/3634-find-mirror-score-of-a-string.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     long long calculateScore(string s) {
         long long total=0,n=s.size();
-        unordered_map<int,vector<int>> ump;
+        vector<vector<int>> ump(26);
 
         for(int i=0;i<n;i++){
             int curr=s[i]-'a', rev=25-curr;
