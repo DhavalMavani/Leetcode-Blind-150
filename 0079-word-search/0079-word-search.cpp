d7c3f6c9279e0127acf4d8773108238d8fa39c5a
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-01-17 15:24:14 +0530

    Time: 184 ms (94.39%), Space: 10.5 MB (84.35%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,26 +1,22 @@
 class Solution {
-private:
-    bool doesExist(int row,int col,int curr){
+public:
+    bool doesExist(int row,int col,int curr,vector<vector<char>>& b, string &w){
         if(curr==w.size()) return true;
 
         if(row>=b.size() || row<0 || col<0 || col>=b[0].size() || b[row][col]!=w[curr]) return false;
 
         char temp=b[row][col];
         b[row][col]='0';
-        bool res= doesExist(row+1,col,curr+1) || doesExist(row,col+1,curr+1) || doesExist(row-1,col,curr+1) || doesExist(row,col-1,curr+1);
+        bool res= doesExist(row+1,col,curr+1,b,w) || doesExist(row,col+1,curr+1,b,w) || doesExist(row-1,col,curr+1,b,w) || doesExist(row,col-1,curr+1,b,w);
         b[row][col]=temp;
 
         return res;
     }
-public:
-    vector<vector<char>> b;
-    string w;
+
     bool exist(vector<vector<char>>& board, string word) {
-        b=board;
-        w=word;
         for (int i=0;i<board.size();i++){
             for(int j=0;j<board[0].size();j++){
-                if(b[i][j]==word[0] && doesExist(i,j,0)){
+                if(board[i][j]==word[0] && doesExist(i,j,0,board,word)){
                     return true;
                 }
             }
