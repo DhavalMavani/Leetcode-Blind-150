e1062b037b22e863724c90c3b5eb84fe00204fa5
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-02-01 17:39:10 +0530

    Time: 0 ms (100%), Space: 8.6 MB (68.43%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x<0) return 0;
 
-        long num=x,rev=0;
+        int rev=0;
 
+        if(x<0 || (x%10==0 && x!=0) ) return 0;
 
-        while (x){
+        while (x>rev ){
             rev= rev*10+x%10;
             x=x/10;
         }
 
-        if ( num==rev ) return true;
-        else return false;
+        if (rev==x || x==rev/10 ) return true;
+        
+        return false;
     }
 };
