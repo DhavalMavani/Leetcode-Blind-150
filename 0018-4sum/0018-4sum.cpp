3bc8b362f4ad6e153df7cfa746796c79d24ad164
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-02-01 22:03:31 +0530

    Time: 27 ms (32.09%), Space: 17.5 MB (61.41%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,45 +1,38 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        if(nums.size()<4){
-            return {};
-        }
+        int n=nums.size();
+        if(n<4) return {};
+
         sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
-        for(int i=0;i<nums.size()-3;i++){
-            if(i>0 && nums[i]==nums[i-1]){
-                continue;
-            }
-            for(int j=i+1;j<nums.size()-2;j++){
-                if(j>i+1 && nums[j]==nums[j-1]){
-                    continue;
-                }
-                long long sum=target-nums[i];
-                sum-=nums[j];
-                int left=j+1,right=nums.size()-1;
+
+        for(int i=0;i<n-3;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            for(int j=i+1;j<n-2;j++){
+                if(j>i+1 && nums[j]==nums[j-1]) continue;
+
+                long sum= (long) target -nums[i] -nums[j];
+
+                int left=j+1,right=n-1;
+
                 while(left<right){
                     if(sum==nums[left]+nums[right]){
                         ans.push_back({nums[i],nums[j],nums[left],nums[right]});
                         left++;
                         right--;
-                        while(left<right && nums[left]==nums[left-1]){
-                            left++;
-                        } 
-                        while(left<right && nums[right]==nums[right+1]){
-                            right--;
-                        } 
+
+                        while(left<right && nums[left]==nums[left-1]) left++;
+
+                        while(left<right && nums[right]==nums[right+1]) right--;
                     }
                     else if(sum<nums[left]+nums[right]){
                         right--;
-                        while(left<right && nums[right]==nums[right+1]){
-                            right--;
-                        } 
+                        while(left<right && nums[right]==nums[right+1]) right--;
                     }
                     else{
                         left++;
-                        while(left<right && nums[left]==nums[left-1]){
-                            left++;
-                        }
+                        while(left<right && nums[left]==nums[left-1]) left++;
                     }
                 }
             }
