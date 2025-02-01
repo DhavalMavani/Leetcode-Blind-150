91924527a88ba44a5dcfb742fd819ee849e8300e
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-02-01 22:04:11 +0530

    Time: 14 ms (90.98%), Space: 17.4 MB (61.41%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -23,17 +23,9 @@ public:
                         right--;
 
                         while(left<right && nums[left]==nums[left-1]) left++;
-
-                        while(left<right && nums[right]==nums[right+1]) right--;
                     }
-                    else if(sum<nums[left]+nums[right]){
-                        right--;
-                        while(left<right && nums[right]==nums[right+1]) right--;
-                    }
-                    else{
-                        left++;
-                        while(left<right && nums[left]==nums[left-1]) left++;
-                    }
+                    else if(sum<nums[left]+nums[right]) right--;
+                    else left++;
                 }
             }
         }
