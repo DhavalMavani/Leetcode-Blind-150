d9671838f00e70d253a8f1380bc18eb433915aa5
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-02-02 14:45:54 +0530

    Time: 3 ms (75.58%), Space: 17.8 MB (91.96%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -2,36 +2,38 @@ class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> graph(numCourses);
-        vector<int> inDegree(numCourses,0);
 
+        vector<int> inDegree(numCourses,0);
         for(auto &i: prerequisites){
             graph[i[1]].emplace_back(i[0]);
             inDegree[i[0]]++;
         }
-
+        
         queue<int> q;
-        vector<int> ans;
+
         for(int i=0;i<numCourses;i++){
-            if(!inDegree[i]){
-                ans.emplace_back(i);
-                q.emplace(i);
-            }    
-        } 
-
-        while(!q.empty()){
-            int c=q.front();
-            q.pop();
-
-            for(auto &i: graph[c]){
-                inDegree[i]--;
-                if( !inDegree[i] ){
-                    ans.emplace_back(i);
-                    q.emplace(i);
-                }
+            if(inDegree[i]==0){
+                q.push(i);
             }
         }
 
+        vector<int> ans;
+
+        while(!q.empty()){
+            int currNode=q.front();
+            q.pop();
+
+            ans.emplace_back(currNode);
+
+            for(auto &i: graph[currNode]){
+                inDegree[i]--;
+                if(inDegree[i]==0) q.push(i);
+            }
+
+        }
+
         if(ans.size()==numCourses) return ans;
+
         return {};
     }
 };
